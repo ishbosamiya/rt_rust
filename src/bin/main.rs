b11efc2a134d0fcf7cea29aa3c7f45edc05a4ba6
ishbosamiya <ishbosamiya@gmail.com>
2021-04-07 23:54:55 +0530

    main: basic image setup

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,20 @@
+use hp_rt::image::{Image, PPM};
+
+use nalgebra_glm as glm;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 1280;
+    let height = 720;
+    let mut image = Image::new(width, height);
+
+    for (_j, row) in image.get_pixels_mut().iter_mut().enumerate() {
+        for (_i, pixel) in row.iter_mut().enumerate() {
+            let _j = height - _j - 1;
+
+            *pixel = glm::vec3(0.3, 0.3, 0.3);
+        }
+    }
+
+    let ppm = PPM::new(&image);
+    ppm.write_to_file("image.ppm").unwrap();
 }
