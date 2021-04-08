5e0ea6c4e8385fa16ce59069d5c1bc84f38474f2
ishbosamiya <ishbosamiya@gmail.com>
2021-04-08 11:03:59 +0530

    main: visualize opengl uv coords

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use rt::image::{Image, PPM};
+use rt::math::Scalar;
 
 use nalgebra_glm as glm;
 
@@ -7,11 +8,16 @@ fn main() {
     let height = 720;
     let mut image = Image::new(width, height);
 
-    for (_j, row) in image.get_pixels_mut().iter_mut().enumerate() {
-        for (_i, pixel) in row.iter_mut().enumerate() {
-            let _j = height - _j - 1;
+    for (j, row) in image.get_pixels_mut().iter_mut().enumerate() {
+        for (i, pixel) in row.iter_mut().enumerate() {
+            let j = height - j - 1;
 
-            *pixel = glm::vec3(0.3, 0.3, 0.3);
+            // use opengl coords, (0.0, 0.0) is center; (1.0, 1.0) is
+            // top right; (-1.0, -1.0) is bottom left
+            let u = ((i as Scalar / (width - 1) as Scalar) - 0.5) * 2.0;
+            let v = ((j as Scalar / (height - 1) as Scalar) - 0.5) * 2.0;
+
+            *pixel = glm::vec3(u, v, 0.0);
         }
     }
 
