ae69c980b5ab74a92999651695b84221e5600ae6
ishbosamiya <ishbosamiya@gmail.com>
2021-04-19 19:04:37 +0530

    threadpool: define Job and documentation

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 
 struct Worker {
@@ -14,13 +15,31 @@ impl Worker {
     }
 }
 
+/// The job to be executed by one of the threads
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+/// A Threadpool consists of a pool of threads to which Jobs
+/// (functions) can be sent to execute on. The threads are 1:1 with
+/// the operating system threads. A job can be sent to be executed and
+/// a free thread picks up the job to complete.
 pub struct ThreadPool {
     num_threads: usize,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// size: number of threads in the pool
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero
     pub fn new(num_threads: usize) -> Self {
         assert_ne!(num_threads, 0);
+
+        let (sender, receiver) = mpsc::channel();
 
         let mut workers = Vec::with_capacity(num_threads);
 
@@ -28,7 +47,18 @@ impl ThreadPool {
             workers.push(Worker::new(id));
         }
 
-        return Self { num_threads };
+        return Self {
+            num_threads,
+            workers,
+            sender,
+        };
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        // TODO(ish)
     }
 
     pub fn get_num_threads(&self) -> usize {
