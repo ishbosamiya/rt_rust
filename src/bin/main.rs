2e2bec68e658718537271eb170d8520d8d0b2cea
ishbosamiya <ishbosamiya@gmail.com>
2021-04-19 23:52:25 +0530

    main: trace_ray: if there is a hit, colour it yellow

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,9 @@
 use rt::camera::Camera;
 use rt::image::{Image, PPM};
+use rt::intersectable::Intersectable;
 use rt::math::{Scalar, Vec3};
 use rt::ray::Ray;
+use rt::sphere::Sphere;
 
 use nalgebra_glm as glm;
 
@@ -16,6 +18,9 @@ fn main() {
     let origin = glm::vec3(0.0, 0.0, 0.0);
     let camera = Camera::new(viewport_height, aspect_ratio, focal_length, origin);
 
+    let scene: Vec<Box<dyn Intersectable>> =
+        vec![Box::new(Sphere::new(glm::vec3(0.0, 0.0, -2.0), 1.5))];
+
     for (j, row) in image.get_pixels_mut().iter_mut().enumerate() {
         for (i, pixel) in row.iter_mut().enumerate() {
             let j = height - j - 1;
@@ -27,9 +32,7 @@ fn main() {
 
             let ray = camera.get_ray(u, v);
 
-            let background_color = get_background_color(&ray, &camera);
-
-            *pixel = background_color;
+            *pixel = trace_ray(&ray, &camera, &scene);
         }
     }
 
@@ -47,4 +50,23 @@ fn get_background_color(ray: &Ray, camera: &Camera) -> Vec3 {
     let y_val = (y_val + 1.0) / 2.0;
 
     return glm::lerp(&color_1, &color_2, y_val);
+}
+
+// x: current point
+// x_prime: previous point
+// x_prime_prime: previous's previous point
+// g: geometry term, 1/(r^2) where r is distance of x_prime to x
+// e: intensity of emitted light by x_prime reaching x
+// i: intensity of light from x_prime to x
+// p: intensity of light scattered from x_prime_prime to x by a patch on surface at x_prime
+fn trace_ray(ray: &Ray, camera: &Camera, scene: &Vec<Box<dyn Intersectable>>) -> Vec3 {
+    let mut val = glm::zero();
+    scene.iter().for_each(|object| {
+        if let Some(_) = object.hit(ray, 0.01, 1000.0) {
+            val = glm::vec3(1.0, 1.0, 0.0);
+        } else {
+            val = get_background_color(ray, camera);
+        }
+    });
+    return val;
 }
