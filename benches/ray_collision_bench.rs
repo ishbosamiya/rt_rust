1aa1371da84c0f850a22470d22f1dbfc8d95f7bb
ishbosamiya <ishbosamiya@gmail.com>
2021-05-17 18:48:39 +0530

    benches: ray_collision: test with multi threading

--- benches/ray_collision_bench.rs
+++ benches/ray_collision_bench.rs
@@ -1,5 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use nalgebra_glm as glm;
+use rand::prelude::*;
 
 use rt::intersectable::Intersectable;
 use rt::math::Scalar;
@@ -9,7 +10,7 @@ use rt::threadpool::ThreadPool;
 
 struct SceneCollisionParams<'a> {
     ray: &'a Ray,
-    scene: &'a Vec<Box<dyn Intersectable>>,
+    scene: &'a Vec<Box<dyn Intersectable + Send + Sync>>,
     t_min: Scalar,
     t_max: Scalar,
 }
@@ -17,7 +18,7 @@ struct SceneCollisionParams<'a> {
 impl<'a> SceneCollisionParams<'a> {
     fn new(
         ray: &'a Ray,
-        scene: &'a Vec<Box<dyn Intersectable>>,
+        scene: &'a Vec<Box<dyn Intersectable + Send + Sync>>,
         t_min: Scalar,
         t_max: Scalar,
     ) -> Self {
@@ -40,24 +41,44 @@ fn scene_collision_st(scene_collision_params: &SceneCollisionParams) {
     });
 }
 
-// fn scene_collision_mt(scene_collision_params: &SceneCollisionParams) {
-//     let ray = scene_collision_params.ray;
-//     let scene = scene_collision_params.scene;
-//     let t_min = scene_collision_params.t_min;
-//     let t_max = scene_collision_params.t_max;
-//     let pool = ThreadPool::new(11);
-//     scene.iter().for_each(|object| {
-//         pool.execute(|| {
-//             object.hit(ray, t_min, t_max);
-//             return;
-//         });
-//     });
-// }
+fn scene_collision_mt(scene_collision_params: &SceneCollisionParams) {
+    let ray = scene_collision_params.ray;
+    let scene = scene_collision_params.scene;
+    let t_min = scene_collision_params.t_min;
+    let t_max = scene_collision_params.t_max;
+    ThreadPool::scoped(12, |scope| {
+        scene.iter().for_each(|object| {
+            scope.execute(|| {
+                object.hit(ray, t_min, t_max);
+            });
+        })
+    });
+}
+
+fn rng_scaled(rng: &mut ThreadRng, scale_factor: f64) -> f64 {
+    return (rng.gen::<f64>() - 0.5) * 2.0 * scale_factor;
+}
+
+fn random_vec3(rng: &mut ThreadRng, scale_factor: f64) -> glm::DVec3 {
+    let x = rng_scaled(rng, scale_factor);
+    let y = rng_scaled(rng, scale_factor);
+    let z = rng_scaled(rng, scale_factor);
+    return glm::vec3(x, y, z);
+}
 
 fn bench_scene_collision(c: &mut Criterion) {
     let ray = Ray::new(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, -1.0));
-    let scene: Vec<Box<dyn Intersectable>> =
+    let mut scene: Vec<Box<dyn Intersectable + Send + Sync>> =
         vec![Box::new(Sphere::new(glm::vec3(0.0, 0.0, -2.0), 1.5))];
+    let mut rng = rand::thread_rng();
+    let num_objects = 10000;
+    let scale_factor = 5.0;
+    for _ in 0..num_objects {
+        scene.push(Box::new(Sphere::new(
+            random_vec3(&mut rng, scale_factor),
+            rng_scaled(&mut rng, scale_factor),
+        )));
+    }
     let (t_min, t_max) = (0.01, 1000.0);
     let scene_collision_params = SceneCollisionParams::new(&ray, &scene, t_min, t_max);
     let mut group = c.benchmark_group("SceneCollision");
@@ -68,13 +89,13 @@ fn bench_scene_collision(c: &mut Criterion) {
             b.iter(|| scene_collision_st(black_box(scene_collision_params)));
         },
     );
-    // group.bench_with_input(
-    //     BenchmarkId::new("multi_threaded", 2),
-    //     &scene_collision_params,
-    //     |b, scene_collision_params| {
-    //         b.iter(|| scene_collision_mt(black_box(scene_collision_params)));
-    //     },
-    // );
+    group.bench_with_input(
+        BenchmarkId::new("multi_threaded", 2),
+        &scene_collision_params,
+        |b, scene_collision_params| {
+            b.iter(|| scene_collision_mt(black_box(scene_collision_params)));
+        },
+    );
     group.finish();
 }
 
