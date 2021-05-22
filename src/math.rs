70c1d1b7af483426626c3af30fe03b955d7176f2
ishbosamiya <ishbosamiya@gmail.com>
2021-05-22 12:12:54 +0530

    fix: random_in_unit_sphere: would give vector only in +ve direction

--- src/math.rs
+++ src/math.rs
@@ -6,7 +6,7 @@ pub type Scalar = f64;
 
 pub fn random_in_unit_sphere() -> Vec3 {
     loop {
-        let p = glm::vec3(random(), random(), random());
+        let p = glm::vec3(random(), random(), random()) * 2.0 - glm::vec3(1.0, 1.0, 1.0);
         if glm::length2(&p) >= 1.0 {
             continue;
         }
