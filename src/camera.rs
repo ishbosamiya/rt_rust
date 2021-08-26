43dabe8639dd7e3364f7b5e44fd9a4e232d583d1
ishbosamiya <ishbosamiya@gmail.com>
2021-08-26 12:01:50 +0530

    camera: clippy suggestions

--- src/camera.rs
+++ src/camera.rs
@@ -13,32 +13,32 @@ impl Camera {
         let viewport_width = viewport_height as f64 * aspect_ratio;
         let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
         let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let camera_plane_center = &origin - Vec3::new(0.0, 0.0, focal_length);
+        let camera_plane_center = origin - Vec3::new(0.0, 0.0, focal_length);
 
-        return Camera {
+        Camera {
             origin,
             horizontal,
             vertical,
             camera_plane_center,
-        };
+        }
     }
 
     pub fn get_origin(&self) -> &Vec3 {
-        return &self.origin;
+        &self.origin
     }
 
     pub fn get_horizontal(&self) -> &Vec3 {
-        return &self.horizontal;
+        &self.horizontal
     }
 
     pub fn get_vertical(&self) -> &Vec3 {
-        return &self.vertical;
+        &self.vertical
     }
 
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
-        return Ray::new(
+        Ray::new(
             self.origin,
-            &self.camera_plane_center + u * &self.horizontal + v * &self.vertical - &self.origin,
-        );
+            self.camera_plane_center + u * self.horizontal + v * self.vertical - self.origin,
+        )
     }
 }
