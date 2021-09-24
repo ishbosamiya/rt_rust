37bbb481acf672605f1d6202a59bd16d254111e5
AdhwaithMenon601 <adhwaith2017.menon@gmail.com>
2021-09-24 11:41:54 +0530

    Modified sampling code

--- src/sampler.rs
+++ src/sampler.rs
@@ -19,222 +19,201 @@ pub fn invertPhi(w : &Vec3, mu : &f64) -> f64 {
 pub fn uniformHemisphere(xi : &Vec2) -> Vec3 {
     let mut phi  = (2.0_f64 * 3.1415926536_f64) * xi.x;
     let mut r = sqrt(max(1.0 - xi.y * xi.y, 0.0));
-    return Vec3::new(cos(phi)*r, sin(phi)*r, xi.y);
+    return Vec3::new(std::cos(phi)*r, std::sin(phi)*r, xi.y);
 }
 
-static inline float uniformHemispherePdf(p : &Vec3) {
+pub fn uniformHemispherePdf(p : &Vec3) -> f64 {
     let INV_TWO_PI = 0.5_f64 * (1.0_f64 / 3.1415926536_f64);
     return INV_TWO_PI;
 }
 
-/*
-static inline Vec3f cosineHemisphere(const Vec2f &xi)
-{
-    float phi = xi.x()*TWO_PI;
-    float r = std::sqrt(xi.y());
+pub fn cosineHemisphere(xi : & Vec2) -> Vec3 {
+    let phi = xi.x * 2.0 * 3.1415926536;
+    let r = sqrt(xi.y);
 
-    return Vec3f(
+    return Vec3(
             std::cos(phi)*r,
             std::sin(phi)*r,
-            std::sqrt(max(1.0f - xi.y(), 0.0f))
+            sqrt(max(1.0 - xi.y, 0.0))
     );
 }
 
-static inline float cosineHemispherePdf(const Vec3f &p)
-{
-    return std::abs(p.z())*INV_PI;
+pub fn cosineHemispherePdf(p : &Vec3) -> f64 {
+    let INV_PI = (1.0_f64 / 3.1415926536_f64)
+    return abs(p.z) * INV_PI;
 }
 
-static inline Vec2f invertCosineHemisphere(const Vec3f &w, float mu)
+pub fn invertCosineHemisphere(w : &Vec3, mu : &f64) -> Vec2
 {
-    return Vec2f(invertPhi(w, mu), max(1.0f - w.z()*w.z(), 0.0f));
+    return glm::vec2::new(invertPhi(w, mu), max(1.0 - w.z * w.z, 0.0));
 }
 
-static inline Vec3f uniformDisk(const Vec2f &xi)
-{
-    float phi = xi.x()*TWO_PI;
-    float r = std::sqrt(xi.y());
-    return Vec3f(std::cos(phi)*r, std::sin(phi)*r, 0.0f);
+pub fn uniformDisk(xi : &Vec2) -> Vec3 {
+    let mut phi = xi.x * 2.0 * 3.1415926536;
+    let r = sqrt(xi.y);
+    return Vec3::new(std::cos(phi)*r, std::sin(phi)*r, 0.0);
 }
 
-static inline float uniformDiskPdf()
-{
-    return INV_PI;
+pub fn uniformDiskPdf() -> f64 {
+    return (1.0_f64 / 3.1415926536_f64);
 }
 
-static inline Vec2f invertUniformDisk(const Vec3f &p, float mu)
-{
-    return Vec2f(invertPhi(p, mu), p.xy().lengthSq());
-}
+// TBD COMPLETE THIS FUNCTION
+// pub fn invertUniformDisk(p : &Vec3, mu : &f64) -> Vec2 {
+//     return glm::vec2::new(invertPhi(p, mu), p.x*p.x + p.y*p.y);
+// }
 
-static inline Vec3f uniformCylinder(Vec2f &xi)
-{
-    float phi = xi.x()*TWO_PI;
-    return Vec3f(
+pub fn uniformCylinder(xi : &Vec2) -> Vec3 {
+    let TWO_PI =  2.0 * 3.1415926536;
+    let mut phi = xi.x * TWO_PI;
+    return Vec3::new(
         std::cos(phi),
         std::sin(phi),
-        xi.y()*2.0f - 1.0f
+        xi.y * 2.0 - 1.0
     );
 }
 
-static inline float uniformCylinderPdf()
-{
-    return INV_PI;
+pub fn uniformCylinderPdf() -> f64 {
+    return (1.0_f64 / 3.1415926536_f64);
 }
 
-static inline Vec3f uniformSphere(const Vec2f &xi)
-{
-    float phi = xi.x()*TWO_PI;
-    float z = xi.y()*2.0f - 1.0f;
-    float r = std::sqrt(max(1.0f - z*z, 0.0f));
+pub fn uniformSphere(xi : &Vec2) -> Vec3 {
+    let TWO_PI = 2.0 * 3.1415926536;
+    let phi = xi.x * TWO_PI;
+    let mut z = xi.y * 2.0 - 1.0;
+    let r = sqrt(max(1.0 - z*z, 0.0));
 
-    return Vec3f(
+    return Vec3::new(
         std::cos(phi)*r,
         std::sin(phi)*r,
         z
     );
 }
 
-static inline float uniformSpherePdf()
-{
-    return INV_FOUR_PI;
+pub fn uniformSpherePdf() -> f64 {
+    return 0.25_f64 * (1.0_f64 / 3.1415926536_f64);
 }
 
-static inline Vec2f invertUniformSphere(const Vec3f &w, float mu)
+pub fn invertUniformSphere(w : &Vec3, mu : &f64) -> Vec2
 {
-    return Vec2f(invertPhi(w, mu), (w.z() + 1.0f)*0.5f);
+    return glm::vec2::new(invertPhi(w, mu), (w.z + 1.0)*0.5);
 }
 
-static inline Vec3f uniformSphericalCap(const Vec2f &xi, float cosThetaMax)
-{
-    float phi = xi.x()*TWO_PI;
-    float z = xi.y()*(1.0f - cosThetaMax) + cosThetaMax;
-    float r = std::sqrt(max(1.0f - z*z, 0.0f));
-    return Vec3f(
-        std::cos(phi)*r,
-        std::sin(phi)*r,
+pub fn uniformSphericalCap(xi : &Vec2, cosThetaMax : &f64) -> Vec3 {
+    let TWO_PI = 2.0 * 3.1415926536;
+    let phi = xi.x * TWO_PI;
+    let mut z = xi.y * (1.0 - cosThetaMax) + cosThetaMax;
+    let r = sqrt(max(1.0 - z*z, 0.0));
+    return Vec3::new(
+        cos(phi)*r,
+        sin(phi)*r,
         z
     );
 }
 
-static inline float uniformSphericalCapPdf(float cosThetaMax)
-{
+pub fn uniformSphericalCapPdf(cosThetaMax : &f64) -> f64 {
+    let INV_TWO_PI = 0.5_f64 * (1.0_f64 / 3.1415926536_f64);
     return INV_TWO_PI/(1.0f - cosThetaMax);
 }
 
-static inline bool invertUniformSphericalCap(const Vec3f &w, float cosThetaMax, Vec2f &xi, float mu)
+pub fn invertUniformSphericalCap(w : &Vec3, cosThetaMax : &f64, xi : &Vec2, mu : &f64) -> bool
 {
-    float xiY = (w.z() - cosThetaMax)/(1.0f - cosThetaMax);
-    if (xiY >= 1.0f || xiY < 0.0f)
+    let mut xiY = (w.z - cosThetaMax)/(1.0 - cosThetaMax);
+    if (xiY >= 1.0 || xiY < 0.0f)
         return false;
 
-    xi = Vec2f(invertPhi(w, mu), xiY);
+    xi = glm::vec2::new(invertPhi(w, mu), xiY);
     return true;
 }
 
-static inline Vec3f phongHemisphere(const Vec2f &xi, float n)
+pub fn phongHemisphere(xi : &Vec2, n : &f64) -> Vec3
 {
-    float phi = xi.x()*TWO_PI;
-    float cosTheta = std::pow(xi.y(), 1.0f/(n + 1.0f));
-    float r = std::sqrt(std::max(1.0f - cosTheta*cosTheta, 0.0f));
-    return Vec3f(std::cos(phi)*r, std::sin(phi)*r, cosTheta);
+    let phi = xi.x * 2.0 * 3.1415926536;
+    let mut cosTheta = xi.y.pow(1.0/(n + 1.0));
+    let r = std::sqrt(max(1.0 - cosTheta*cosTheta, 0.0));
+    return Vec3::new(std::cos(phi)*r, std::sin(phi)*r, cosTheta);
 }
 
-static inline float phongHemispherePdf(const Vec3f &v, float n)
-{
-    return INV_TWO_PI*(n + 1.0f)*std::pow(v.z(), n);
+pub fn phongHemispherePdf(v : &Vec3, n : &f64) -> f64 {
+    let INV_TWO_PI = 0.5_f64 * (1.0_f64 / 3.1415926536_f64);
+    return INV_TWO_PI*(n + 1.0)*v.z.pow(n);
 }
 
-static inline Vec2f invertPhongHemisphere(const Vec3f &w, float n, float mu)
-{
-    return Vec2f(invertPhi(w, mu), std::pow(w.z(), n + 1.0f));
+pub fn invertPhongHemisphere(w : &Vec3, n : &f64, mu : &f64) -> Vec2 {
+    return Vec2::new(invertPhi(w, mu), w.z.pow(n + 1.0));
 }
 
-static inline Vec2f uniformTriangleUv(const Vec2f &xi)
-{
-    float uSqrt = std::sqrt(xi.x());
-    float alpha = 1.0f - uSqrt;
-    float beta = (1.0f - xi.y())*uSqrt;
+pub fn uniformTriangleUv(xi : &Vec2) -> Vec2 {
+    let mut uSqrt = xi.x.sqrt();
+    let alpha = 1.0 - uSqrt;
+    let beta = (1.0 - xi.y)*uSqrt;
 
-    return Vec2f(alpha, beta);
+    return glm::vec2::new(alpha, beta);
 }
 
-static inline Vec2f invertUniformTriangleUv(const Vec2f &uv)
-{
-    return Vec2f(sqr(1.0f - uv.x()), 1.0f - uv.y()/(1.0f - uv.x()));
+fn uniformTriangle(xi : &Vec2, a : &Vec3, b : &Vec3, c : &Vec3) -> Vec3 {
+    let mut uv : Vec2 = uniformTriangleUv(xi);
+    return a*uv.x + b*uv.y + c*(1.0 - uv.x - uv.y);
 }
 
-static inline Vec3f uniformTriangle(const Vec2f &xi, const Vec3f& a, const Vec3f& b, const Vec3f& c)
-{
-    Vec2f uv = uniformTriangleUv(xi);
-    return a*uv.x() + b*uv.y() + c*(1.0f - uv.x() - uv.y());
-}
-
-static inline float uniformTrianglePdf(const Vec3f &a, const Vec3f &b, const Vec3f &c)
-{
-    return 2.0f/((b - a).cross(c - a).length());
-}
-
-static inline float powerHeuristic(float pdf0, float pdf1)
-{
+pub fn powerHeuristic(pdf0 : &f64, pdf1 : &f64) -> f64 {
     return (pdf0*pdf0)/(pdf0*pdf0 + pdf1*pdf1);
 }
 
-static inline Vec3f projectedBox(const Box3f &box, const Vec3f &direction, float faceXi, const Vec2f &xi)
-{
-    Vec3f diag = box.diagonal();
-    float areaX = diag.y()*diag.z()*std::abs(direction.x());
-    float areaY = diag.z()*diag.x()*std::abs(direction.y());
-    float areaZ = diag.x()*diag.y()*std::abs(direction.z());
+/// TODO Combine box code with below
+pub fn projectedBox(box : &Box, direction : &Vec3, faceXi : &f64, xi : &Vec2) -> Vec3 {
+    let mut diag: Box = box.diagonal();
+    let areaX = diag.y*diag.z*std::abs(direction.x);
+    let areaY = diag.z*diag.x*std::abs(direction.y);
+    let areaZ = diag.x*diag.y*std::abs(direction.z);
 
-    float u = faceXi*(areaX + areaY + areaZ);
+    let u : f64 = faceXi * (areaX + areaY + areaZ);
 
-    Vec3f result;
+    let mut result : Vec3::new(0.0, 0.0, 0.0);
     if (u < areaX) {
-        result.x() = direction.x() < 0.0f ? box.max().x() : box.min().x();
-        result.y() = box.min().y() + diag.y()*xi.x();
-        result.z() = box.min().z() + diag.z()*xi.y();
+        result.x = box.max.x if direction.x < 0.0 else box.min.x;
+        result.y = box.min().y + diag.y * xi.x;
+        result.z = box.min().z + diag.z * xi.y;
     } else if (u < areaX + areaY) {
-        result.y() = direction.y() < 0.0f ? box.max().y() : box.min().y();
-        result.z() = box.min().z() + diag.z()*xi.x();
-        result.x() = box.min().x() + diag.x()*xi.y();
+        result.y = box.max().y if direction.y < 0.0 else box.min().y;
+        result.z = box.min().z + diag.z * xi.x;
+        result.x = box.min().x + diag.x * xi.y;
     } else {
-        result.z() = direction.z() < 0.0f ? box.max().z() : box.min().z();
-        result.x() = box.min().x() + diag.x()*xi.x();
-        result.y() = box.min().y() + diag.y()*xi.y();
+        result.z = box.max().z if direction.z < 0.0 else box.min().z;
+        result.x = box.min().x + diag.x * xi.x;
+        result.y = box.min().y + diag.y * xi.y;
     }
 
     return result;
 }
 
-static inline float projectedBoxPdf(const Box3f &box, const Vec3f &direction)
-{
-    Vec3f diag = box.diagonal();
-    float areaX = diag.y()*diag.z()*std::abs(direction.x());
-    float areaY = diag.z()*diag.x()*std::abs(direction.y());
-    float areaZ = diag.x()*diag.y()*std::abs(direction.z());
+pub fn projectedBoxPdf(box : &Box, direction : &Vec3) -> f64 {
+    let mut diag : Vec3 = box.diagonal();
+    let areaX = diag.y * diag.z * direction.x.abs();
+    let areaY = diag.z * diag.x * direction.y.abs();
+    let areaZ = diag.x * diag.y * direction.z.abs();
 
-    return 1.0f/(areaX + areaY + areaZ);
+    return 1.0 / (areaX + areaY + areaZ);
 }
 
-static inline bool invertProjectedBox(const Box3f &box, const Vec3f &o, const Vec3f &d, float &faceXi, Vec2f &xi, float mu)
-{
-    Vec3f invD = 1.0f/d;
-    Vec3f relMin((box.min() - o));
-    Vec3f relMax((box.max() - o));
+pub fn invertProjectedBox(box : &Box, o : &Vec3, d : &Vec3, faceXi : &f64, xi : &Vec2, mu : &f64) -> bool {
+    let invD: Vec3 = 1.0/d;
+    let mut relMin: Vec3::new((box.min() - o),(box.min() - o),(box.min() - o));
+    let mut relMax: Vec3::new((box.max() - o),(box.max() - o),(box.max() - o));
 
-    float ttMin = 0, ttMax = 1e30f;
-    int dim = -1;
-    for (int i = 0; i < 3; ++i) {
+    let ttMin : f64 = 0;
+    let ttMax : f64 = 1e30_f64;
+    let dim = -1;
+    for i in 0..3) {
         if (invD[i] >= 0.0f) {
             ttMin = max(ttMin, relMin[i]*invD[i]);
-            float x = relMax[i]*invD[i];
+            let mut x: f64 = relMax[i]*invD[i];
             if (x < ttMax) {
                 ttMax = x;
                 dim = i;
             }
         } else {
-            float x = relMin[i]*invD[i];
+            let mut x: f64 = relMin[i]*invD[i];
             if (x < ttMax) {
                 ttMax = x;
                 dim = i;
@@ -244,18 +223,18 @@ static inline bool invertProjectedBox(const Box3f &box, const Vec3f &o, const Ve
     }
 
     if (ttMin <= ttMax) {
-        Vec3f diag = box.diagonal();
-        int dim1 = (dim + 1) % 3;
-        int dim2 = (dim + 2) % 3;
+        let mut diag : Vec3 = box.diagonal();
+        let dim1 = (dim + 1) % 3;
+        let dim2 = (dim + 2) % 3;
 
-        xi = Vec2f(
+        xi = Vec2::new(
             (o[dim1] + d[dim1]*ttMax - box.min()[dim1])/diag[dim1],
             (o[dim2] + d[dim2]*ttMax - box.min()[dim2])/diag[dim2]
         );
 
-        float areaX = diag.y()*diag.z()*std::abs(d.x());
-        float areaY = diag.z()*diag.x()*std::abs(d.y());
-        float areaZ = diag.x()*diag.y()*std::abs(d.z());
+        let mut areaX = diag.y * diag.z * d.x.abs();
+        let mut areaY = diag.z * diag.x * d.y.abs();
+        let mut areaZ = diag.x * diag.y * d.z.abs();
 
         if (dim == 0)
             faceXi = mu*areaX/(areaX + areaY + areaZ);
@@ -269,7 +248,6 @@ static inline bool invertProjectedBox(const Box3f &box, const Vec3f &o, const Ve
     }
     return false;
 }
-*/
 
 
 /// May need to move to a different file
