d7bfb8bc32772b8305c0cd5f023cc9dfc2a22484
vikshith-s <vikshith@hotmail.com>
2021-10-12 19:57:03 +0530

    lambert bsdf done. TODO : test

--- src/lambert.rs
+++ src/lambert.rs
@@ -1,5 +1,9 @@
 use crate::glm;
+use rand::Rng;
 use crate::bsdf::BSDF;
+extern crate rand;
+use crate::sampler;
+use rand::thread_rng;
 
 pub struct Lambert {
     pub reflectance: f64
@@ -11,6 +15,8 @@ impl BSDF for Lambert {
             reflectance: 1.0_f64
         }
     }
+
+
     fn eval(
         &self,
         _l: &glm::DVec3,
@@ -20,13 +26,30 @@ impl BSDF for Lambert {
         _y: &glm::DVec3,
     ) -> glm::DVec3 {
         // Simple function according to disney brdf
-        let val: f64 = self.reflectance / std::f64::consts::PI;
-        glm::vec3(val, val, val)
+        let a :f64 = glm::one_over_pi();
+        return glm::vec3(a,a,a);
     }
-    fn sample(&self, 
-        _out : &glm::DVec3, 
-        _vertex : &glm::DVec3
+
+
+
+    fn sample( &self, 
+        outgoing : &glm::DVec3, 
+        vertex : &glm::DVec3,
+        
     ) -> glm::DVec3 {
-        glm::zero()
+        let mut rng = thread_rng();
+        let x: f64 = rng.gen_range(0.0..1.0);
+        let y: f64 = rng.gen_range(0.0..1.0);
+        let s = glm::vec2(x, y);
+        let normal = vertex.normalize();
+        let mut incoming = sampler::cosine_hemisphere(&s);
+        incoming = incoming[0] * outgoing + incoming[1] * normal + incoming[2] * vertex;
+        let wi = sampler::cosine_hemisphere(&s);
+
+        
+        let prob : f64 = wi[1] * 0.31830988_f64 ;
+
+        assert!(prob > 0.0_f64);
+        incoming
     }
 }
