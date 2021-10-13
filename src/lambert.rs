d5a516198548de41ea1566f9130d640d56316558
ishbosamiya <ishbosamiya@gmail.com>
2021-10-13 12:08:49 +0530

    lambert: clippy changes

--- src/lambert.rs
+++ src/lambert.rs
@@ -16,19 +16,6 @@ impl BSDF for Lambert {
         }
     }
 
-    fn eval(
-        &self,
-        _l: &glm::DVec3,
-        _v: &glm::DVec3,
-        _n: &glm::DVec3,
-        _x: &glm::DVec3,
-        _y: &glm::DVec3,
-    ) -> glm::DVec3 {
-        // Simple function according to disney brdf
-        let a: f64 = glm::one_over_pi();
-        return glm::vec3(a, a, a);
-    }
-
     fn sample(&self, outgoing: &glm::DVec3, vertex: &glm::DVec3) -> glm::DVec3 {
         let mut rng = thread_rng();
         let x: f64 = rng.gen_range(0.0..1.0);
@@ -39,9 +26,22 @@ impl BSDF for Lambert {
         incoming = incoming[0] * outgoing + incoming[1] * normal + incoming[2] * vertex;
         let wi = sampler::cosine_hemisphere(&s);
 
-        let prob: f64 = wi[1] * 0.31830988_f64;
+        let prob: f64 = wi[1] * std::f64::consts::FRAC_1_PI;
 
         assert!(prob > 0.0_f64);
         incoming
+    }
+
+    fn eval(
+        &self,
+        _l: &glm::DVec3,
+        _v: &glm::DVec3,
+        _n: &glm::DVec3,
+        _x: &glm::DVec3,
+        _y: &glm::DVec3,
+    ) -> glm::DVec3 {
+        // Simple function according to disney brdf
+        let a: f64 = glm::one_over_pi();
+        glm::vec3(a, a, a)
     }
 }
