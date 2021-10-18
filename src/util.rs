5bf3df10d0d1234280534e8360a188040f996863
ishbosamiya <ishbosamiya@gmail.com>
2021-10-18 12:01:37 +0530

    util: functions to apply model matrix to vec2, vec3, and normal

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use crate::glm;
+
 /// Get offset to struct member, similar to `offset_of` in C/C++
 /// From https://stackoverflow.com/questions/40310483/how-to-get-pointer-offset-in-bytes/40310851#40310851
 #[macro_export]
@@ -11,4 +13,20 @@ macro_rules! offset_of {
 pub fn str_to_cstr(string: &str) -> &std::ffi::CStr {
     return std::ffi::CStr::from_bytes_with_nul(string.as_bytes())
         .expect("ensure there is a '\\0' at the end of the string");
+}
+
+fn append_one(vec: &glm::DVec3) -> glm::DVec4 {
+    glm::vec4(vec[0], vec[1], vec[2], 1.0)
+}
+
+pub fn vec2_apply_model_matrix(v: &glm::DVec2, model: &glm::DMat4) -> glm::DVec3 {
+    glm::vec4_to_vec3(&(model * append_one(&glm::vec2_to_vec3(v))))
+}
+
+pub fn vec3_apply_model_matrix(v: &glm::DVec3, model: &glm::DMat4) -> glm::DVec3 {
+    glm::vec4_to_vec3(&(model * append_one(v)))
+}
+
+pub fn normal_apply_model_matrix(normal: &glm::DVec3, model: &glm::DMat4) -> glm::DVec3 {
+    vec3_apply_model_matrix(normal, &glm::inverse_transpose(*model))
 }
