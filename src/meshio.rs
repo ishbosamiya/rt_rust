0995985fa424d00a358b81162669be1a376fff62
AdhwaithMenon601 <adhwaith2017.menon@gmail.com>
2021-10-20 15:01:15 +0530

    removed extra lines

--- src/meshio.rs
+++ src/meshio.rs
@@ -130,53 +130,6 @@ impl MeshIO {
         let mut contents = String::new();
         file_data.read_to_string(&mut contents).unwrap();
 
-        let split = contents.split('\n');
-        let split_string: Vec<&str> = split.collect();
-        // let string_vec: Vec<_> = split_string.iter().map(|&x| x.split('\n')).collect();
-
-        for iter in split_string {
-            let line: Vec<&str> = iter.split(' ').collect();
-            if line[0] == "o" {
-                // Add to starting
-            }
-            else if line[0] == "v" {
-                let v1 = line[1].parse::<f64>().unwrap();
-                let v2 = line[2].parse::<f64>().unwrap();
-                let v3 = line[3].parse::<f64>().unwrap();
-                positions.push(glm::vec3(v1, v2, v3));
-                face_starting[0] += 1;
-            }
-            else if line[0] == "vt" {
-                face_has_uv = true;
-                let v1 = line[1].parse::<f64>().unwrap();
-                let v2 = line[2].parse::<f64>().unwrap();
-                uvs.push(glm::vec2(v1, v2));
-                face_starting[1] += 1;
-            }
-            else if line[0] == "vn"{
-                face_has_normal = true;
-                let v1 = line[1].parse::<f64>().unwrap();
-                let v2 = line[2].parse::<f64>().unwrap();
-                let v3 = line[3].parse::<f64>().unwrap();
-                normals.push(glm::vec3(v1, v2, v3));
-                face_starting[2] += 1;
-            }
-            else if line[0] == "f" {
-                for (pos, e) in line.iter().enumerate() {
-                    let mut face_row = Vec::new();
-                    if pos != 0 {
-                        let face_split: Vec<&str> = e.split('/').collect();
-                        let f1 = face_split[0].parse::<usize>().unwrap();
-                        let f2 = face_split[1].parse::<usize>().unwrap();
-                        let f3 = face_split[2].parse::<usize>().unwrap();
-                        face_row.push((f1, f2, f3));
-                    }
-                    face_indices.push(face_row);
-                }
-
-            }
-        }
-
         let reader = BufReader::new(fin);
 
         for line in reader.lines() {
