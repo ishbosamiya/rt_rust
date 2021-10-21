a17e6f41a864310db2ea29adf8c0cc583fd83820
ishbosamiya <ishbosamiya@gmail.com>
2021-10-21 19:19:10 +0530

    main: progress: store time to finish

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -180,14 +180,16 @@ fn ray_trace_quit_render(
 #[derive(Debug, Clone)]
 pub struct Progress {
     progress: f64,
-    instant: std::time::Instant,
+    instant: Instant,
+    finished_time: Option<Duration>,
 }
 
 impl Progress {
     pub fn new() -> Self {
         Self {
             progress: 0.0,
-            instant: std::time::Instant::now(),
+            instant: Instant::now(),
+            finished_time: None,
         }
     }
 
@@ -196,16 +198,24 @@ impl Progress {
     }
 
     pub fn set_progress(&mut self, prog: f64) {
+        if (prog - 1.0).abs() < f64::EPSILON {
+            self.finished_time = Some(self.instant.elapsed());
+        }
         self.progress = prog;
     }
 
     pub fn reset(&mut self) {
         self.progress = 0.0;
-        self.instant = std::time::Instant::now();
+        self.instant = Instant::now();
+        self.finished_time = None;
     }
 
     pub fn get_elapsed_time(&self) -> f64 {
-        self.instant.elapsed().as_secs_f64()
+        if (self.progress - 1.0).abs() < f64::EPSILON {
+            self.finished_time.unwrap().as_secs_f64()
+        } else {
+            self.instant.elapsed().as_secs_f64()
+        }
     }
 
     pub fn get_remaining_time(&self) -> f64 {
@@ -271,6 +281,7 @@ use std::rc::Rc;
 use std::sync::mpsc::{self, Receiver};
 use std::sync::{Arc, RwLock};
 use std::thread::{self, JoinHandle};
+use std::time::{Duration, Instant};
 
 use egui::{FontDefinitions, FontFamily, TextStyle};
 use egui_glfw::EguiBackend;
