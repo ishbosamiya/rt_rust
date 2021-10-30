c5f0a05b2eced1808de65131d41b190e02b7a02b
AdhwaithMenon601 <adhwaith2017.menon@gmail.com>
2021-10-30 11:11:10 +0530

    Checking for cli args

--- testing_framework/src/main.rs
+++ testing_framework/src/main.rs
@@ -1,20 +1,27 @@
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{value_t, App, Arg};
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
-struct ExecTest {
-    name: String,
+pub fn is_exec_in_path(path: &Path) -> bool {
+    path.exists()
 }
 
-impl ExecTest {
-    // fn new() -> Self {
-    //     let app = App::new("test-executable")
-    //         .version("1.0")
-    //         .about("Tests Command Line Arguements")
-    //         .author("Nobody");
-    //     let name = matches.value_of("name").expect("Was Required");
-    // }
-}
+// struct ExecTest {
+//     name: String,
+// }
+
+// impl ExecTest {
+//     // fn new() -> Self {
+//     //     let app = App::new("test-executable")
+//     //         .version("1.0")
+//     //         .about("Tests Command Line Arguements")
+//     //         .author("Nobody");
+//     //     let name = matches.value_of("name").expect("Was Required");
+//     // }
+// }
 
 fn main() {
     //println!("Main");
@@ -22,25 +29,39 @@ fn main() {
         .version("1.0")
         .about("Tests Command Line Arguements")
         .author("Nobody")
-        .arg(Arg::with_name("Arg1").index(1).help("Checking Executable"))
+        .arg(
+            Arg::with_name("config")
+                .index(1)
+                .short("c")
+                .long("config")
+                .help("Config")
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("exec")
+                .required(true)
+                .short("e")
+                .index(2)
+                .help("Executable Location"),
+        )
+        .arg(
+            Arg::with_name("pwd")
+                .required(true)
+                .short("d")
+                .index(3)
+                .help("Give Directory"),
+        )
         .get_matches();
 
     // Checking for executable file
     // Unsure of how file is sent
     // let permissions = file.metadata()?.permissions();
-    // let is_executable = permissions.mode() & 0o111 != 0;
-}
-
-#[cfg(test)]
-mod test {
-
-    #[test]
-    fn test_no_args() {
-        // HelloArgs::new_from(["exename"].iter()).unwrap_err();
-    }
-
-    #[test]
-    fn test_incomplete_name() {
-        // HelloArgs::new_from(["exename", "--name"].iter()).unwrap_err();
-    }
+    // let is_exec = permissions.mode() & 0o111 != 0;
+    let config = app.value_of("config").unwrap();
+    let file_name = app.value_of("exec").unwrap();
+    let dir_name = app.value_of("pwd").unwrap();
+    let mut path = PathBuf::from(dir_name);
+    path.push(file_name);
+    assert!(is_exec_in_path(path.as_path()));
 }
