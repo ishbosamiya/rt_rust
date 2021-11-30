0df9438c4a5f666e95c3fe60ce423d045f7c174b
ishbosamiya <ishbosamiya@gmail.com>
2021-11-30 13:02:54 +0530

    benches: bvh_benchmark: bvh of different tree types and axes

--- benches/bvh_benchmark.rs
+++ benches/bvh_benchmark.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rt::bvh::BVHTree;
 use rt::glm;
 
-fn bvh_ray_cast_benchmark(c: &mut Criterion) {
+fn build_scene(bvh_epsilon: f64, bvh_tree_type: u8, bvh_axis: u8) -> BVHTree<usize> {
     let cube_verts = vec![
         glm::vec3(1.0, 1.0, 1.0),
         glm::vec3(1.0, 1.0, -1.0),
@@ -31,10 +31,10 @@ fn bvh_ray_cast_benchmark(c: &mut Criterion) {
     ];
 
     let plane_verts = vec![
-        glm::vec3(1.0, 0.0, 1.0),
-        glm::vec3(1.0, 0.0, -1.0),
-        glm::vec3(-1.0, 0.0, 1.0),
-        glm::vec3(-1.0, 0.0, -1.0),
+        glm::vec3(4.0, -1.0, 4.0),
+        glm::vec3(4.0, -1.0, -4.0),
+        glm::vec3(-4.0, -1.0, 4.0),
+        glm::vec3(-4.0, -1.0, -4.0),
     ];
 
     let plane = vec![
@@ -42,26 +42,23 @@ fn bvh_ray_cast_benchmark(c: &mut Criterion) {
         [plane_verts[0], plane_verts[3], plane_verts[1]],
     ];
 
-    let bvh_epsilon = 0.01;
-    let bvh_tree_type = 4;
-    let bvh_axis = 6;
-    let bvh = {
-        let mut bvh = BVHTree::<usize>::new(
-            cube.len() + plane.len(),
-            bvh_epsilon,
-            bvh_tree_type,
-            bvh_axis,
-        );
-        cube.iter()
-            .chain(plane.iter())
-            .enumerate()
-            .for_each(|(index, verts)| {
-                bvh.insert(index, verts);
-            });
-        bvh.balance();
-        bvh
-    };
+    let mut bvh = BVHTree::<usize>::new(
+        cube.len() + plane.len(),
+        bvh_epsilon,
+        bvh_tree_type,
+        bvh_axis,
+    );
+    cube.iter()
+        .chain(plane.iter())
+        .enumerate()
+        .for_each(|(index, verts)| {
+            bvh.insert(index, verts);
+        });
+    bvh.balance();
+    bvh
+}
 
+fn bvh_ray_cast_benchmark(c: &mut Criterion) {
     let ray_origin = glm::vec3(0.0, 0.0, 0.0);
     let camera_focal_length = 50.0;
     let camera_plane_center = glm::vec3(0.0, 0.0, camera_focal_length);
@@ -79,12 +76,249 @@ fn bvh_ray_cast_benchmark(c: &mut Criterion) {
         })
         .collect();
 
+    let bvh_2_6 = build_scene(0.01, 2, 6);
+    let bvh_4_6 = build_scene(0.01, 4, 6);
+    let bvh_8_6 = build_scene(0.01, 8, 6);
+    let bvh_16_6 = build_scene(0.01, 16, 6);
+    let bvh_24_6 = build_scene(0.01, 24, 6);
+    let bvh_31_6 = build_scene(0.01, 31, 6);
+
+    let bvh_2_8 = build_scene(0.01, 2, 8);
+    let bvh_4_8 = build_scene(0.01, 4, 8);
+    let bvh_8_8 = build_scene(0.01, 8, 8);
+    let bvh_16_8 = build_scene(0.01, 16, 8);
+    let bvh_24_8 = build_scene(0.01, 24, 8);
+    let bvh_31_8 = build_scene(0.01, 31, 8);
+
+    let bvh_2_14 = build_scene(0.01, 2, 14);
+    let bvh_4_14 = build_scene(0.01, 4, 14);
+    let bvh_8_14 = build_scene(0.01, 8, 14);
+    let bvh_16_14 = build_scene(0.01, 16, 14);
+    let bvh_24_14 = build_scene(0.01, 24, 14);
+    let bvh_31_14 = build_scene(0.01, 31, 14);
+
+    let bvh_2_26 = build_scene(0.01, 2, 26);
+    let bvh_4_26 = build_scene(0.01, 4, 26);
+    let bvh_8_26 = build_scene(0.01, 8, 26);
+    let bvh_16_26 = build_scene(0.01, 16, 26);
+    let bvh_24_26 = build_scene(0.01, 24, 26);
+    let bvh_31_26 = build_scene(0.01, 31, 26);
+
     let mut ray_dirs_index = 0;
-    c.bench_function("bvh_ray_cast", |b| {
-        b.iter(|| bvh.ray_cast::<fn((&glm::DVec3, &glm::DVec3), usize) -> Option<rt::bvh::RayHitData<usize, ()>>, ()>(criterion::black_box(ray_origin), criterion::black_box(ray_dirs[ray_dirs_index]), criterion::black_box(None)));
-        ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
-    });
+
+    let mut group = c.benchmark_group("BVH Ray Cast");
+
+    {
+        group.bench_function("bvh_2_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_2_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_4_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_4_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_8_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_8_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_16_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_16_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_24_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_24_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_31_6", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_31_6, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+    }
+
+    {
+        group.bench_function("bvh_2_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_2_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_4_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_4_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_8_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_8_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_16_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_16_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_24_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_24_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_31_8", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_31_8, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+    }
+
+    {
+        group.bench_function("bvh_2_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_2_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_4_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_4_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_8_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_8_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_16_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_16_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_24_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_24_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_31_14", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_31_14, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+    }
+
+    {
+        group.bench_function("bvh_2_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_2_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_4_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_4_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_8_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_8_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_16_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_16_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_24_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_24_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+
+        group.bench_function("bvh_31_26", |b| {
+            ray_dirs_index = 0;
+            b.iter(|| {
+                bvh_ray_cast(&bvh_31_26, ray_origin, ray_dirs[ray_dirs_index]);
+                ray_dirs_index = (ray_dirs_index + 1) % ray_dirs.len();
+            });
+        });
+    }
+
+    group.finish();
 }
 
-criterion_group!(benches, bvh_ray_cast_benchmark);
+#[inline(always)]
+fn bvh_ray_cast(bvh: &BVHTree<usize>, co: glm::DVec3, dir: glm::DVec3) {
+    bvh.ray_cast::<fn((&glm::DVec3, &glm::DVec3), usize) -> Option<rt::bvh::RayHitData<usize, ()>>, ()>(criterion::black_box(co), criterion::black_box(dir), criterion::black_box(None));
+}
+
+fn bvh_benchmark_config() -> Criterion {
+    Criterion::default()
+}
+
+criterion_group!(name = benches; config = bvh_benchmark_config(); targets = bvh_ray_cast_benchmark);
 criterion_main!(benches);
