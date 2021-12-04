c8fecd1aa56406df5b63ba9e7628e8825e07a7bf
ishbosamiya <ishbosamiya@gmail.com>
2021-12-05 00:29:08 +0530

    benches: ray_trace_scene_benchmark: increase image width&height
    By increasing the width and height, there are more pixels that will
    have to intersect with the scene and thus actually test the ray
    tracing speed.
    
    With embree: [215.44 ms 216.48 ms 217.53 ms]
    Without embree: [2.5241 s 2.5487 s 2.5722 s]
    
    Thus, with embree, it is a 11.8x improvement :)

--- benches/ray_trace_benchmark.rs
+++ benches/ray_trace_benchmark.rs
@@ -8,15 +8,17 @@ use rt::progress::Progress;
 
 fn ray_trace_scene_benchmark(c: &mut Criterion) {
     let arguments =
-        InputArguments::read_string("--width 100 --height 100 --trace-max-depth 25 --samples 1 --environment ./test_scenes/hdrs/white_background.hdr --textures ./test_scenes/materialball/materialball_scene_backdrop.png --shader-texture backdrop,0 --obj-files ./test_scenes/materialball/materialball_scene.obj --object-shader backdrop,backdrop --object-shader light,emission --object-shader diffuse_object,diffuse --object-shader material_object,test_material --rt-file ./test_scenes/materialball/diffuse.rt".to_string());
+        InputArguments::read_string("--width 1000 --height 1000 --trace-max-depth 25 --samples 1 --environment ./test_scenes/hdrs/white_background.hdr --textures ./test_scenes/materialball/materialball_scene_backdrop.png --shader-texture backdrop,0 --obj-files ./test_scenes/materialball/materialball_scene.obj --object-shader backdrop,backdrop --object-shader light,emission --object-shader diffuse_object,diffuse --object-shader material_object,test_material --rt-file ./test_scenes/materialball/diffuse.rt".to_string());
 
     let (ray_trace_params, scene, shader_list, texture_list, environment) =
         arguments.generate_render_info();
 
-    assert_eq!(ray_trace_params.get_width(), 100);
-    assert_eq!(ray_trace_params.get_height(), 100);
+    assert_eq!(ray_trace_params.get_width(), 1000);
+    assert_eq!(ray_trace_params.get_height(), 1000);
     assert_eq!(ray_trace_params.get_trace_max_depth(), 25);
     assert_eq!(ray_trace_params.get_samples_per_pixel(), 1);
+
+    scene.write().unwrap().rebuild_bvh_if_needed(0.01);
 
     c.bench_function("ray_trace_scene", |b| {
         b.iter(|| {
@@ -34,5 +36,9 @@ fn ray_trace_scene_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, ray_trace_scene_benchmark);
+fn ray_trace_scene_benchmark_config() -> Criterion {
+    Criterion::default().sample_size(20)
+}
+
+criterion_group!(name = benches; config = ray_trace_scene_benchmark_config(); targets = ray_trace_scene_benchmark);
 criterion_main!(benches);
