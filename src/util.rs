70ebdfef39f77034f86d9f9c038e52c2e7bd83bb
ishbosamiya <ishbosamiya@gmail.com>
2021-12-05 15:47:46 +0530

    util: remove offset_of macro, use memoffset crate instead
    Dereferencing a null pointer is undefined behaviour and this needs a
    lot more complex way to figure out the offset. It is easier to use
    memoffset crate instead.
    
    One interesting thing to note is that, it is possible to implement
    offset_of macro if the struct has a default implementation and is not
    costly to create. Vulkano (the crate) seems to do this.

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,5 @@
 use crate::glm;
 
-/// Get offset to struct member, similar to `offset_of` in C/C++
-/// From <https://stackoverflow.com/questions/40310483/how-to-get-pointer-offset-in-bytes/40310851#40310851>
-#[macro_export]
-macro_rules! offset_of {
-    ($ty:ty, $field:ident) => {
-        &(*(std::ptr::null() as *const $ty)).$field as *const _ as usize
-    };
-}
-
 /// str to CStr
 pub fn str_to_cstr(string: &str) -> &std::ffi::CStr {
     return std::ffi::CStr::from_bytes_with_nul(string.as_bytes())
